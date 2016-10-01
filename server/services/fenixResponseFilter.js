8477d124c0df9092bb6af6f6842d37b82aa9cd50
joaotomazio <jdtomazio@gmail.com>
2016-10-01 12:11:30 +0100

    bug fix on fenixResponseFilter

--- server/services/fenixResponseFilter.js
+++ server/services/fenixResponseFilter.js
@@ -4,21 +4,24 @@ module.exports = function (data, day, callback){
 
 	var filtered = collection.filter(data[0].events, function(item){return item.classPeriod.start.includes(day)});
 	var ordered = collection.sortBy(filtered, [function(o) { return o.classPeriod.start; }, function(o) { return o.classPeriod.end; }]);
-	
+
 	var output = [];
 
 	function getCourseAcronym(fullname) {
 		var acronym = '';
 
-		for(i=0; i<fullname.length; i++){
-			if((fullname[i] >= 'A') && (fullname[i] <= 'Z')) acronym.concat(str[i]);
-		}
-
+		for(i=0; i<fullname.length; i++)
+			if((fullname[i] >= 'A') && (fullname[i] <= 'Z')) acronym += fullname[i];
+	
 		return acronym;
 	}
 
-	for(i=0; i<ordered.length; i++){
-		output.push({'name': getCourseAcronym(ordered[i].course.name), 'start': ordered[i].classPeriod.start.substring(11), 'end': ordered[i].classPeriod.end.substring(11)});
+	for(j=0; j<ordered.length; j++){
+		var brick = {name: getCourseAcronym(ordered[j].course.name),
+					start: ordered[j].classPeriod.start.substring(11),
+					end: ordered[j].classPeriod.end.substring(11)};
+
+		output.push(brick);
 	}
 	
 	callback(null, output);
