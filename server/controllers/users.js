d1ca9f7ef34aab6fa9c2d1a9811df7e05d249967
tonyfreire <tonifreire93@gmail.com>
2016-10-01 15:30:53 +0000

    add api to get users info

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,7 +1,22 @@
 const Users  = require("../models/user");
 
 module.exports.getUserInfo  = function(req,res) {
+	let id = req.params.id
+	if (req.params.id==="me"){
+		id=req.user.username;
+	}
+	Users.findOne({username:id},function(err,user){
+		let users={
+				username: 		user.username,
+				name: 			user.name,
+				driver: 		user.driver,
+				location: 		user.location,
+				campus: 		user.campus
+			}
+		res.json(users);
+		
 
+	});
 	
 
 };
