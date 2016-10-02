47bad578029bc29aa5bb8620de08955be1757c32
johnytiago <joaosao10@gmail.com>
2016-10-02 01:50:05 +0100

    Messaging working

--- app/reducers/user.js
+++ app/reducers/user.js
@@ -1,28 +1,102 @@
 export default function reducer(state={
-    user : {
-        id: null,
-        username: null
-    },
-    fetching: false,
-    fetched: false,
-    error: null
+  user : {
+    tecnicoID: null,
+    username: null,
+  },
+  msgs: { },
+  phones: { },
+  socket: null,
+  fetching: false,
+  fetched: false,
+  sending: false,
+  sent: false,
+  error: null
 }, action){
-    if(action.type === "FETCH_USER")
-        return {...state, fetching: true}
-    if(action.type === "FETCH_USER_FULFILLED")
-        return {
-            ...state,
-            user: {
-                id : action.payload.id,
-                username : action.payload.username
-            },
-            fetching: false,
-            fetched: true,
-            error: null
-        }
-    if(action.type === "FETCH_USER_REJECTED")
-        return {...state, fetching: false, error: action.payload}
-    if(action.type === "SET_USERNAME")
-        return {...state, name: action.payload}
-    return state
+  if(action.type === "FETCH_USER")
+    return {...state, fetching: true}
+  if(action.type === "FETCH_USER_FULFILLED")
+    return {
+      ...state,
+      user: {
+        tecnicoID : action.payload.tecnicoID,
+        username : action.payload.username
+      },
+      fetching: false,
+      fetched: true,
+      error: null
+    }
+  if(action.type === "FETCH_USER_REJECTED")
+    return {...state, fetching: false, error: action.payload}
+
+  if(action.type === "SOCKET_CONNECT_USER")
+    return {...state, fetching: true }
+  if(action.type === "SOCKET_CONNECT_USER_FULFILLED")
+    return {
+      ...state,
+      socket : action.payload,
+      fetching: false,
+      fetched: true,
+      error: null
+    }
+  if(action.type === "SOCKET_CONNECT_USER_REJECTED")
+    return {
+      ...state,
+      fetching: false,
+      error: action.payload
+    }
+
+  if(action.type === "SOCKET_SEND_MESSAGE")
+    return {...state, sending: true }
+  if(action.type === "SOCKET_SEND_MESSAGE_FULFILLED"){
+    let { from, to, msg, when } = action.payload
+    if (!state.msgs[to])
+      state.msgs[to] = [{ from, to, msg, when }]
+    state.msgs[to] = [ ...state.msgs[to], { from, to, msg, when }]
+    return {
+      ...state,
+      msgs: state.msgs, 
+      sending: false,
+      sent: true,
+      error: null
+    }
+  }
+  if(action.type === "SOCKET_SEND_MESSAGE_REJECTED")
+    return {
+      ...state,
+      fetching: false,
+      error: action.payload
+    }
+
+  if(action.type === "SOCKET_NEW_MESSAGE_FULFILLED"){
+    let { from, to, msg, when } = action.payload
+    if (!state.msgs[from])
+      state.msgs[from] = [{ from, to, msg, when }]
+    state.msgs[from] = [ ...state.msgs[to], { from, to, msg, when }]
+    return {
+      ...state,
+      msgs: state.msgs,
+      sending: false,
+      sent: true,
+      error: null
+    }
+  }
+
+  if(action.type === "SOCKET_PHONE_REQUEST") {
+    let tecnicoID = action.payload
+    phones[tecnicoID] = { pending: true }
+    return {
+      ...state, 
+      phones 
+    }
+  }
+  if(action.type === "SOCKET_PHONE_REQUEST_ERROR") {
+    return {...state, error: action.payload }
+  }
+  if(action.type === "SOCKET_PHONE_REQUEST_FULFILLED"){
+    let { from, phone: num } = action.payload
+    phones[from] = { phone: num, pending: false }
+    return {...state, phones }
+  }
+
+  return state
 }
--- app/web/components/App.js
+++ app/web/components/App.js
@@ -1,16 +1,60 @@
-
 import React, {Component} from 'react'
 import { Grid } from 'react-bootstrap'
+import { connect } from 'react-redux'
+
+import { connect_socket, send_message } from '../../actions/socket'
 
 import Header from './Header'
 
+@connect((store)=>{
+  return {
+    tecnicoID : store.user.user.tecnicoID,
+    socket : store.user.socket,
+  }
+})
 export default class App extends Component{
 
-    render() {
-        return <div>
-            <Header />
-            <Grid>{this.props.children}</Grid>
+  constructor() {
+    super()
+    this.state = { from: Math.floor(Math.random()*10000) }
+  }
+
+  componentDidMount = () => {
+    //this.props.dispatch(connect_socket(this.props.tecnicoID))
+    // usar o tecnicoID da API, random por agora 
+    this.props.dispatch(connect_socket(this.state.from))
+  }
+
+  handleChangeI = event => {
+    this.setState({from: event.target.value});
+  }
+  handleChangeII = event => {
+    this.setState({to: event.target.value});
+  }
+
+  handleSummit = event => {
+    event.preventDefault()
+    this.props.dispatch(send_message(this.props.socket, this.state.from, this.state.to, "Hello from the other side"))
+  } 
+
+  render() {
+    return ( 
+      <div>
+        <Header />
+        FROM:<input type="text" 
+          value={this.state.from}
+          onChange={this.handleChangeI.bind(this)}
+        />
+        TO:<input type="text" 
+          value={this.state.to}
+          onChange={this.handleChangeII.bind(this)}
+        />
+        <button onClick={this.handleSummit.bind(this)}>
+          Send message
+        </button>
+        <Grid>{this.props.children}</Grid>
       </div>
-    }
+    )
+  }
 
 }
